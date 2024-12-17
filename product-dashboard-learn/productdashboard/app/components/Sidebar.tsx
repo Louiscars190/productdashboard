8744db654271bf825a93ca800cdc5398f91c3474
louiscars190 <louiscars170@gmail.com>
2024-12-17 18:27:15 -0500

    hi

---
+++ product-dashboard-learn/productdashboard/app/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+
+const Sidebar = () =>
+{
+    return (
+     <h1 className="justify">hi</h1>   
+    )
+}
+
+export default Sidebar;
---
+++ product-dashboard-learn/productdashboard/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+import Link from "next/link";
+import Sidebar from "@/app/components/Sidebar"
+
+
+export default function dashboard() {
+    return (
+        <Sidebar />
+    );
+}
