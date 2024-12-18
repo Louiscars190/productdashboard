519db17207982fc9633b950f336c943be386a994
louiscars190 <louiscars170@gmail.com>
2024-12-17 20:47:43 -0500

    working on auth logic

--- product-dashboard-learn/productdashboard/app/login/signup/page.tsx
+++ product-dashboard-learn/productdashboard/app/login/signup/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import Link from "next/link";
 import Head from "next/head";
 import type { NextPage } from "next";
-import { useState } from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "@/firebase";
 
-const signup: NextPage = () => {
+const SignUp: NextPage = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
+    const HandleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("");
+        setSuccess("");
 
+    }
 
     return(
         <div>
@@ -18,13 +26,13 @@ const signup: NextPage = () => {
                 <title>Sign-up Page</title>
             </Head>
             <main>
-                <form className="flex flex-col justify-center items-center h-screen" method="POST">
-                    <div className="border p-10 rounded">
+                <form className="flex flex-col justify-center items-center h-screen" onSubmit={HandleSubmit}>
+                    <div className="border-4 p-10 rounded-3xl">
                         <h2 className="text-2xl font-bold mb-6 text-center">Sign up</h2>
                         <div className="block mb-2 justify-center text-center align-center">
                             <label htmlFor="name">Name: </label>
                             <input 
-                            className="rounded p-1 mb-6" 
+                            className="rounded p-1 mb-6 placeholder-gray-400 text-black" 
                             type="text" 
                             id="name" 
                             placeholder="Name"
@@ -38,13 +46,32 @@ const signup: NextPage = () => {
                             <br/>   
 
                             <label htmlFor="email">Email : </label>
-                            <input className="rounded p-1 mb-6" type="email" id="email" name="email" required></input>
-                        
+                            <input 
+                            className="rounded p-1 mb-6 placeholder-gray-400 text-black" 
+                            type="text" 
+                            id="email" 
+                            placeholder="Email"
+                            name="email" 
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
                         
                             <br/>
 
                             <label htmlFor="password">Password: </label>
-                            <input className="rounded p-1 mb-6" type="password" id="password" name="password" required></input>
+                            <input 
+                            className="rounded p-1 mb-6 placeholder-gray-400 text-black" 
+                            type="password" 
+                            id="password" 
+                            placeholder="Password"
+                            name="password" 
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            >
+                            </input>
 
                             <br/>
                             <button className="border rounded p-2 w-ful" type="submit">Sign up now</button>
@@ -56,7 +83,7 @@ const signup: NextPage = () => {
     )
 }
 
-export default signup;
+export default SignUp;
 
 
 
